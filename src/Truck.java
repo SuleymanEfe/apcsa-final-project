a1b248ff709ed499521b695b65423f568746351e
ella <ella@localhost>
2023-05-16 18:38:03 -0700

    pls work i beg

--- src/Truck.java
+++ src/Truck.java
@@ -1,5 +1,3 @@
-import javax.xml.validation.Validator;
-
 public class Truck extends Vehicle {
     public Truck(int id, int type, String color, int price, int numberOfSeats, int speed, int year, int mileage) {
         super(id, type, color, price, numberOfSeats, speed, year, mileage);
