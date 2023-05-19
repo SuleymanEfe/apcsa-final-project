3e61ff30b1e160f6b0a464f71d956954d6c0123e
Efe Celik <47013879+SuleymanEfe@users.noreply.github.com>
2023-05-19 01:08:26 -0700

    Update Vehicle.java

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -19,73 +19,70 @@ public class Vehicle {
         this.mileage = mileage;
     }
 
-    // Post-condition: Returns the ID of the vehicle.
+    /**
+    * @return the ID of the vehicle.
+    */
     public int getID() {
         return id;
     }
 
-    // Post-condition: Returns the type of the vehicle.
+    /**
+    * @return the type of the vehicle in int format (See constants.java).
+    */
     public int getType() {
         return type;
     }
 
-    // Post-condition: Returns the color of the vehicle.
+    /**
+    * @return the color of the vehicle.
+    */
     public String getColor() {
         return color;
     }
 
-    // Post-condition: Returns the price of the vehicle.
+    /**
+    * @return the price of the vehicle.
+    */
     public int getPrice() {
         return price;
     }
 
-    // Post-condition: Returns the number of seats of the vehicle.
+    /**
+    * @return the number of seats of the vehicle.
+    */
     public int getNumberOfSeats() {
         return numberOfSeats;
     }
 
-    // Post-condition: Returns the speed of the vehicle.
+    /**
+    * @return the maximum speed of the vehicle.
+    */
     public int getSpeed() {
         return speed;
     }
 
-    // Post-condition: Returns the year of the vehicle.
+    /**
+    * @return the year of the vehicle.
+    */
     public int getYear() {
         return year;
     }
 
-    // Post-condition: Returns the mileage of the vehicle.
+    /**
+    * @return the mileage of the vehicle.
+    */
     public int getMileage() {
         return mileage;
     }
 
-    // Post-condition: Returns a string representation of the vehicle, including its ID, type, year, color, price, number of seats, speed, and mileage.
+    /**
+    * @return a string representation of the vehicle, including its ID, type, year, color, price, number of seats, speed, and mileage.
+    */
     public String toString() {
-        String vehicleType = "";
+        String[] vehicleTypes = {"SUV", "Motorcycle", "Truck", "Sedan", "Sports Car"};
+        String vehicleType = vehicleTypes[type];
 
-        switch (type) {
-            case 0:
-                // SUV
-                vehicleType = "SUV";
-                break;
-            case 1:
-                // MOTORCYCLE
-                vehicleType = "Motorcycle";
-                break;
-            case 2:
-                // TRUCK
-                vehicleType = "Truck";
-                break;
-            case 3:
-                // SEDAN
-                vehicleType = "Sedan";
-                break;
-            case 4:
-                // SPORTS CAR
-                vehicleType = "Sports Car";
-                break;
-        }
-
-        return "Vehicle #" + id + ": \n\tType: " + vehicleType + "\n\tYear: " + year + "\n\tColor: " + color + "\n\tPrice: $" + price + "\n\tNumber of seats: " + numberOfSeats + "\n\tSpeed: " + speed + " mph\n\tMileage: " + mileage + " miles";
+        return "Vehicle #" + id + ": \n\tType: " + vehicleType + "\n\tYear: " + year + "\n\tColor: " + color + "\n\tPrice: $" + price + "\n\tNumber of seats: " + 
+            numberOfSeats + "\n\tSpeed: " + speed + " mph\n\tMileage: " + mileage + " miles";
     }
 }
